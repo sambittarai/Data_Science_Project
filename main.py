b7f90bbe5fd12e05f7b2a1772d3b523ea041d006
Sambit <sambitarai17@gmail.com>
2025-02-18 12:48:38 +0100

    Model Trainer Implementation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 try:
@@ -29,6 +30,16 @@ try:
 	logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
 	obj = DataTransformationTrainingPipeline()
 	obj.initiate_data_transformation()
+	logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+	logger.execution(e)
+	raise e
+
+STAGE_NAME="Model Training Stage"
+try:
+	logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+	obj = ModelTrainerTrainingPipeline()
+	obj.initiate_model_training()
 	logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
 	logger.execution(e)
