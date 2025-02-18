30dc647150f7efeb0431856c5d460e9bf470b2d4
Sambit <sambitarai17@gmail.com>
2025-02-18 10:51:41 +0100

    Data Transformation completed

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -1,7 +1,7 @@
 from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
 
-from src.datascience.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
+from src.datascience.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -27,19 +27,28 @@ class ConfigurationManager:
 
         )
         return data_ingestion_config
-
-
-    def get_data_validation_config(self)-> DataValidationConfig:
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
         schema = self.schema.COLUMNS
-        
+
         create_directories([config.root_dir])
 
-        data_validation_config=DataValidationConfig(
+        data_validation_config = DataValidationConfig(
             root_dir=config.root_dir,
             STATUS_FILE=config.STATUS_FILE,
-            unzip_data_dir=config.unzip_data_dir,
-            all_schema=schema
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
         )
+
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config=self.config.data_transformation
+        create_directories([config.root_dir])
+        data_transformation_config=DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path
+        )
+        return data_transformation_config
 
